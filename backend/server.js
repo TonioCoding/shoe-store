5c20577770cc29dff6b60121b5086cbaec72555c
TonioCoding <ToneBone7960@gmail.com>
2023-12-09 16:17:38 -0500

    formatted code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config({path: '../.env'})
+dotenv.config({ path: "../.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
