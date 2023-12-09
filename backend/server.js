962231bef94f6c8436486cf48a139e7dbcce0ed8
TonioCoding <ToneBone7960@gmail.com>
2023-12-09 14:46:05 -0500

    configuring backend, .env port is undefined

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Backend server is running on port: http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
