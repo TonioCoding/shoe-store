0e3f28dfa43f5503c459fcb8b5f63c085badbfa7
TonioCoding <ToneBone7960@gmail.com>
2023-12-12 11:47:17 -0500

    configuring backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel";
+import createToken from "../utils/createToken";
+
+// description   auth user and sets token
+// route
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    createToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
