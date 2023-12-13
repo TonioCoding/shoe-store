671c8ab03236e257c6ee2f1c5edcec2527cd3600
TonioCoding <ToneBone7960@gmail.com>
2023-12-13 11:29:38 -0500

    fixed routing error

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js"
 
+// Init app and port
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,20 +15,24 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Middleware binding
-app.use(notFound);
-app.use(errorHandler);
+// Root
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 // Routes binding
 app.use("/api/users", userRoutes);
+
+// Middleware binding
+app.use(notFound);
+app.use(errorHandler);
 
 // Main functionality
 app.listen(port, () => {
   console.log(`Backend server is running on port: http://localhost:${port}`);
 });
+
+// Database connection
 connectToDatabase();
 
-// Root
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+
