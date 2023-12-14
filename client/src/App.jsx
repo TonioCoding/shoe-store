32f0f889165bbfd95a04629515243af6bcd26c7a
TonioCoding <ToneBone7960@gmail.com>
2023-12-14 15:24:13 -0500

    fixed frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
-//import { PrivateRoute } from "./components/PrivateRoute.js";
 import HomePage from "./pages/HomePage.jsx";
+//import { PrivateRoute } from "./components/PrivateRoute.js";
 
-function App() {
-  <BrowserRouter>
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<HomePage />}/>
-    </Routes>
-  </BrowserRouter>;
-}
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
