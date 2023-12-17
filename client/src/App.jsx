2cc31a013496e08164f532b402a2c1e8f2159f59
TonioCoding <ToneBone7960@gmail.com>
2023-12-16 19:09:45 -0500

    configured navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import Test from "./pages/Test"
+import Test from "./pages/Test";
 //import { PrivateRoute } from "./components/PrivateRoute.js";
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <NavBar />
-        </div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
