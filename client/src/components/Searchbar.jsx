4f0970653d470263cd491292dbe590fc3742ac3b
TonioCoding <ToneBone7960@gmail.com>
2023-12-19 12:08:07 -0500

    configured border color

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -3,7 +3,7 @@ import { Input } from "@material-tailwind/react";
 const NavSearchBar = () => {
   return (
     <div className="w-72">
-      <Input label="Search" color="black" style={{outlineColor: 'red'}}/>
+      <Input label="Search" color="black" style={{WebkitBorderBeforeColor: "black", borderBlockColor: "black", borderLeftColor: "black", borderRightColor: "black"}}/>
     </div>
   );
 }
