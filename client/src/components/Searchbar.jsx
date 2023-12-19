e0172054926c36ed6ccafaf77f9f8c8cad22371e
TonioCoding <ToneBone7960@gmail.com>
2023-12-19 12:08:27 -0500

    formatted code

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,11 +1,20 @@
 import { Input } from "@material-tailwind/react";
- 
+
 const NavSearchBar = () => {
   return (
     <div className="w-72">
-      <Input label="Search" color="black" style={{WebkitBorderBeforeColor: "black", borderBlockColor: "black", borderLeftColor: "black", borderRightColor: "black"}}/>
+      <Input
+        label="Search"
+        color="black"
+        style={{
+          WebkitBorderBeforeColor: "black",
+          borderBlockColor: "black",
+          borderLeftColor: "black",
+          borderRightColor: "black",
+        }}
+      />
     </div>
   );
-}
+};
 
 export default NavSearchBar;
