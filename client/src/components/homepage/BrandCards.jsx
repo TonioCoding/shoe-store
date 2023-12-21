829962b10baa33a2c4b7f2dff2efb9d017fee740
TonioCoding <ToneBone7960@gmail.com>
2023-12-21 18:56:37 -0500

    configuring home page

--- client/src/components/homepage/BrandCards.jsx
+++ client/src/components/homepage/BrandCards.jsx
@@ -3,9 +3,9 @@ import HomeCard from "../HomePageCard";
 const BrandCards = () => {
   return (
     <div className="flex justify-between">
-      <HomeCard />
-      <HomeCard />
-      <HomeCard />
+      <HomeCard src="https://thrivemyway.com/wp-content/uploads/2022/03/Adidas-Logo-Shoe-Brands.png" brandName="Adidas"/>
+      <HomeCard src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/40-Famous-Shoe-Logos/Nike.png" brandName="Nike" />
+      <HomeCard src="https://customeazy.com/cdn/shop/products/JORDAN1winglogoironon_530x@2x.png?v=1666164933" brandName="Jordan"/>
     </div>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import BrandsSection from "../components/homepage/BrandsSection";
+import BrandCards from "../components/homepage/BrandCards";
 
 const HomePage = () => {
   return (
     <main>
       <BrandsSection/>
+      <BrandCards/>
     </main>
   );
 };
