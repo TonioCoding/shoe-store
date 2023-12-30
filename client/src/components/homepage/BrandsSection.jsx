346e61f33dee4c76c5312059e64f9a78c3348565
TonioCoding <ToneBone7960@gmail.com>
2023-12-30 16:04:21 -0500

    added component and styled UI

--- client/src/components/homepage/BrandsSection.jsx
+++ client/src/components/homepage/BrandsSection.jsx
@@ -3,10 +3,12 @@ import LogoSlider from "./BrandLogoSlider";
 const BrandsSection = () => {
   return (
     <div>
-      <div className="flex justify-between m-6 max-w-full">
-        <LogoSlider/>
-        <h1 className="font-lt text-2xl">STYLE AND CULTURE = THE SHOE</h1>
-        <p></p>
+      <div className="flex justify-around max-w-full m-5">
+        <LogoSlider />
+        <div className="flex flex-col mx-10">
+          <h1 className="font-lt text-2xl">STYLE AND CULTURE = THE SHOE</h1>
+          <p className="font-rt text-black m-3">Search by our current sales</p>
+        </div>
       </div>
       <h2 className="font-lt text-2xl text-center max-w-full mt-10">
         DISCOVER WITH BELOVED BRANDS
---
+++ client/src/components/homepage/SaleCard.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+
+const SaleCard = (props) => {
+  return (
+    <Card className="mt-6 w-96">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          {props.heading}
+        </Typography>
+        <Typography>
+          {props.text}
+        </Typography>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Link className="inline-block">
+          <Button size="sm" variant="text" className="flex items-center gap-2">
+            Learn More
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default SaleCard;
