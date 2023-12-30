1ee32c94dd48fc0ea240e649326b3ea4c71fd492
TonioCoding <ToneBone7960@gmail.com>
2023-12-30 13:38:24 -0500

    added footer

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import BrandsSection from "../components/homepage/BrandsSection";
 import BrandCards from "../components/homepage/BrandCards";
 import MultiLongCards from "../components/homepage/MultiLongCards";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
@@ -10,12 +11,17 @@ const HomePage = () => {
         <h2 className="text-center font-lt max-w-full text-2xl">
           TRENDING BRANDS
         </h2>
-        <hr className="border-black my-5"/>
+        <hr className="border-black my-5" />
       </div>
       <BrandCards />
-      <hr className="border-black my-5 mb-20"/>
-      <h2 className="text-center font-lt max-w-full text-2xl mb-10">Featured</h2>
+      <hr className="border-black my-5 mb-20" />
+      <h2 className="text-center font-lt max-w-full text-2xl mb-10">
+        Featured
+      </h2>
       <MultiLongCards/>
+      <hr className="border-black my-20 h-px" />
+      {/*FOOTER MARK*/}
+      <Footer />
     </main>
   );
 };
