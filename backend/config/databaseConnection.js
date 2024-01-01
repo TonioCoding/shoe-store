d7943248c8dd4ef89026003ab19ee96b6d956b24
TonioCoding <ToneBone7960@gmail.com>
2024-01-01 17:56:42 -0500

    added concurrently script, db connect issues

--- backend/config/databaseConnection.js
+++ backend/config/databaseConnection.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config({ path: "../../.env" });
+
 
 async function connectToDatabase() {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(
-      `Connected to MongoDB database via URI: ${connect.connection.host}`
+      `Connected to MongoDB database via URI: ${connect.connection.host}`,
     );
   } catch (error) {
     console.log(`Error; ${error.message}`);
