56469cbf244acb5ad724cf1be792560d07c88f0e
TonioCoding <ToneBone7960@gmail.com>
2024-01-05 14:01:56 -0500

    fixed branded car section

--- client/src/components/homepage/BrandCardsSection.jsx
+++ client/src/components/homepage/BrandCardsSection.jsx
@@ -6,7 +6,7 @@ const BrandCards = () => {
       <h2 className="text-center font-lt max-w-full text-2xl my-5 mt-10">
         TRENDING BRANDS
       </h2>
-      <div className=" flex justify-center md:flex-wrap ">
+      <div className="flex flex-wrap justify-center">
         <HomeCard
           src="https://static.vecteezy.com/system/resources/previews/010/994/239/original/adidas-logo-black-symbol-clothes-design-icon-abstract-football-illustration-with-white-background-free-vector.jpg"
           brandName="Adidas"
