7baa56004bf92f30c79d5a31de9fab1db832374d
TonioCoding <ToneBone7960@gmail.com>
2024-01-04 23:23:02 -0500

    fixed featured card

--- client/src/components/homepage/FeaturedCard.jsx
+++ client/src/components/homepage/FeaturedCard.jsx
@@ -2,12 +2,18 @@
 
 const FeaturedCard = (props) => {
   return (
-    <img
-      src={props.imgUrl}
-      alt="card-img"
-      className="object-cover border-gray-400 border-2"
-      width={"400px"}
-    />
+    <div>
+      <div className="w-[400px] inline-block">
+        <img
+          src={props.imgUrl}
+          alt="card-img"
+          className="object-cover border-gray-400 border-2"
+          width={"100%"}
+        />
+      </div>
+      <h1 className="font-lt text-center text-2xl my-3">{props.heading}</h1>
+      <p className="font-rt text-center">{props.text}</p>
+    </div>
   );
 };
 
--- client/src/components/homepage/FeaturedSection.jsx
+++ client/src/components/homepage/FeaturedSection.jsx
@@ -6,10 +6,10 @@ const FeaturedSection = () => {
       <h2 className="text-center font-lt max-w-full text-2xl mb-10">
         Featured
       </h2>
-      <div className="flex justify-around">
-        <FeaturedCard imgUrl="https://www.originalfook.com/cdn/shop/products/814CCE65-D048-4900-9E7C-C451E87C36A6_grande.jpg?v=1582554774" />
-        <FeaturedCard imgUrl="https://cdn.myshoptet.com/usr/www.sneakergear.eu/user/shop/detail/171-7_af1-metal-plate-special.jpg?65118278" />
-        <FeaturedCard imgUrl="https://cdn.myshoptet.com/usr/www.sneakergear.eu/user/shop/detail/171-7_af1-metal-plate-special.jpg?65118278" />
+      <div className="flex justify-around mb-20">
+        <FeaturedCard imgUrl="https://www.originalfook.com/cdn/shop/products/814CCE65-D048-4900-9E7C-C451E87C36A6_grande.jpg?v=1582554774" text="Protection" heading="Protection"/>
+        <FeaturedCard imgUrl="https://blankarchive.com/wp-content/uploads/2022/04/AF1-Barb-04.jpg" text="Shoe accessories" heading="Shoe accessories"/>
+        <FeaturedCard imgUrl="https://i.pinimg.com/474x/d9/72/74/d97274552c4927cf090d62b4d7034464.jpg" text="trending shoes" heading="Trending shoes"/>
       </div>
     </div>
   );
