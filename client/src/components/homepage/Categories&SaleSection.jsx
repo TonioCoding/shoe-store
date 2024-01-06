5830b9834e315250d5891876056099c5a411f04d
TonioCoding <ToneBone7960@gmail.com>
2024-01-06 14:41:20 -0500

    styled UI and added documentation

--- client/src/components/homepage/Categories&SaleSection.jsx
+++ client/src/components/homepage/Categories&SaleSection.jsx
@@ -8,7 +8,7 @@ const BrandsSection = () => {
         <LogoSlider />
         <div className="flex flex-col mx-10">
           <h1 className="font-lt text-2xl mb-10 m-5"><span className="text-red-600">STYLE </span> AND CULTURE = THE SHOE</h1>
-          <h2 className="font-lt text-lg text-center">Find a pair</h2>
+          <h2 className="font-lt text-xl text-center">Find a pair</h2>
           <SaleCard heading="Sales" text="Discover our current sales" />
           <SaleCard heading="Categories" text="Discover by various categories" />
         </div>
