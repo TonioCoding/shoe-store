9fdd6ddfe8f649bfc123c898b06ca1ce839c6aea
TonioCoding <ToneBone7960@gmail.com>
2024-01-06 12:58:18 -0500

    commented out import that restricted app rendering

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { Input } from "@material-tailwind/react";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+//import { FaMagnifyidngGlass } from "react-icons/fa6";
 
 const NavSearchBar = () => {
   return (
