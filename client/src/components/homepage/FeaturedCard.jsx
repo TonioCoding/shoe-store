defc57d7bd886e63c79127f7337494b89564a54d
TonioCoding <ToneBone7960@gmail.com>
2024-01-06 13:21:03 -0500

    configuring featured section

--- client/src/components/homepage/FeaturedCard.jsx
+++ client/src/components/homepage/FeaturedCard.jsx
@@ -2,16 +2,16 @@
 
 const FeaturedCard = (props) => {
   return (
-    <div className="cursor-pointer">
-      <div className="w-full inline-block">
+    <div className="cursor-pointer flex flex-col justify-center items-center my-7">
+      <div className="w-[50vw] inline-flex items-center justify-center">
         <img
           src={props.imgUrl}
           alt="card-img"
-          className=" w-full object-scale-down object-center border-gray-400 border-2"
+          className="w-full object-cover object-center border-gray-400 border-2"
         />
       </div>
       <h1 className="font-lt text-center text-xl my-1">{props.heading}</h1>
-      <p className="font-rt text-center mb-10 text-sm">{props.text}</p>
+      <p className="font-rt text-center text-sm">{props.text}</p>
     </div>
   );
 };
--- client/src/components/homepage/FeaturedSection.jsx
+++ client/src/components/homepage/FeaturedSection.jsx
@@ -6,7 +6,7 @@ const FeaturedSection = () => {
       <h2 className="text-center font-lt max-w-full text-2xl mb-10">
         FEATURED
       </h2>
-      <div className="flex flex-col justify-around mb-20">
+      <div className="flex flex-col justify-around mb-10">
         <FeaturedCard imgUrl="https://www.originalfook.com/cdn/shop/products/814CCE65-D048-4900-9E7C-C451E87C36A6_grande.jpg?v=1582554774" text="Protection and maintence products for your sneakers" heading="Protection"/>
         <FeaturedCard imgUrl="https://blankarchive.com/wp-content/uploads/2022/04/AF1-Barb-04.jpg" text="Express your style with various accessories" heading="Shoe accessories"/>
         <FeaturedCard imgUrl="https://i.pinimg.com/474x/d9/72/74/d97274552c4927cf090d62b4d7034464.jpg" text="See what shoes are trending" heading="Trending shoes"/>
