c060e428a272c7debf2c749d547de7718b2532bb
TonioCoding <ToneBone7960@gmail.com>
2024-01-09 15:40:05 -0500

    resized searchbar

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Input } from "@material-tailwind/react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+
 const NavSearchBar = () => {
   return (
     <div className="flex justify-center w-full">
@@ -11,10 +12,15 @@ const NavSearchBar = () => {
           borderBlockColor: "black",
           borderLeftColor: "black",
           borderRightColor: "black",
-          width: "100%",
+          width: "115%",
         }}
       />
-      {<FaMagnifyingGlass color="black" className="w-[5%] cursor-pointer items-center flex justify-center h-10 mx-2" />}
+      {
+        <FaMagnifyingGlass
+          color="black"
+          className="w-[5%] cursor-pointer items-center flex justify-center h-10 mx-2"
+        />
+      }
     </div>
   );
 };
