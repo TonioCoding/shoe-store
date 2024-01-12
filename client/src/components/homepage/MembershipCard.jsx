f39815698afd64379fdedbd4aa5b608ba10ed5dd
TonioCoding <ToneBone7960@gmail.com>
2024-01-12 16:32:11 -0500

    created membership section foundation

--- client/src/components/homepage/MembershipCard.jsx
+++ client/src/components/homepage/MembershipCard.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
 
-
 const MembershipCard = (props) => {
   return (
-    <div>
-        <div className="w-[50vw] h-[70vh] cursor-pointer flex justify-center items-center">
-            <img src={props.imgUrl} alt="img" className="object-cover object-center"/>
-        </div>
+    <div className="mx-10 mb-10 bg-black border-gray-700 border-2">
+      <div className="w-[35vw] h-[70vh] cursor-pointer flex justify-center items-center ">
+        <img
+          src={props.imgUrl}
+          alt="img"
+          className="object-cover object-center "
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MembershipCard
+export default MembershipCard;
--- client/src/components/homepage/MembershipSection.jsx
+++ client/src/components/homepage/MembershipSection.jsx
@@ -2,13 +2,16 @@ import MembershipCard from "./MembershipCard";
 
 const MembershipSection = () => {
   return (
-  <div className="membership-slider">
-    <MembershipCard imgUrl=""/>
-    <MembershipCard imgUrl=""/>
-    <MembershipCard imgUrl=""/>
-    <MembershipCard imgUrl=""/>
-    <MembershipCard imgUrl=""/>
-  </div>
+    <div>
+      <h1 className="text-center font-lt text-3xl mb-20">Membership</h1>
+      <div className="membership-slider">
+        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
+        <MembershipCard imgUrl="https://animemangastore.com/wp-content/uploads/2022/12/men-shoes-sneakers-male-mens-casual-shoe_main-3-1-600x600.jpg" />
+        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
+        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
+        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
+      </div>
+    </div>
   );
 };
 
