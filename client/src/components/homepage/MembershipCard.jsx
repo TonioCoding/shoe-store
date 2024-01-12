32669fa1e9f31d4aeedc690bffb07c49c46ab87c
TonioCoding <ToneBone7960@gmail.com>
2024-01-12 18:12:05 -0500

    styling membership section

--- client/src/components/homepage/MembershipCard.jsx
+++ client/src/components/homepage/MembershipCard.jsx
@@ -2,12 +2,12 @@
 
 const MembershipCard = (props) => {
   return (
-    <div className="mx-10 mb-10 bg-black border-gray-700 border-2">
+    <div className="mx-10 mb-10 ">
       <div className="w-[35vw] h-[70vh] cursor-pointer flex justify-center items-center ">
         <img
           src={props.imgUrl}
           alt="img"
-          className="object-cover object-center "
+          className="object-cover object-center border-gray-700 border-2"
         />
       </div>
     </div>
--- client/src/components/homepage/MembershipSection.jsx
+++ client/src/components/homepage/MembershipSection.jsx
@@ -1,15 +1,18 @@
 import MembershipCard from "./MembershipCard";
-
+import ButtonRounded from "../RoundedButton";
 const MembershipSection = () => {
   return (
     <div>
       <h1 className="text-center font-lt text-3xl mb-20">Membership</h1>
       <div className="membership-slider">
-        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
+        <MembershipCard imgUrl="https://cdn.govexec.com/media/img/cd/2023/11/17/11172023USPS/open-graph.jpg" />
         <MembershipCard imgUrl="https://animemangastore.com/wp-content/uploads/2022/12/men-shoes-sneakers-male-mens-casual-shoe_main-3-1-600x600.jpg" />
         <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
         <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
         <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
+      </div>
+      <div className="flex justify-center mt-10">
+        <ButtonRounded text="Become a member!"/>
       </div>
     </div>
   );
