9da5053526e0ec50a79a74df93b6b17bc7fec28b
TonioCoding <ToneBone7960@gmail.com>
2024-01-12 22:24:21 -0500

    styled membership section

--- client/src/components/homepage/MembershipSection.jsx
+++ client/src/components/homepage/MembershipSection.jsx
@@ -2,17 +2,37 @@ import MembershipCard from "./MembershipCard";
 import ButtonRounded from "../RoundedButton";
 const MembershipSection = () => {
   return (
-    <div>
-      <h1 className="text-center font-lt text-3xl mb-20">Membership</h1>
+    <div className="w-full">
+      <div className="mb-10">
+        <h1 className="text-center font-lt text-3xl mb-5">Membership</h1>
+        <p className="text-center font-rt text-lg">
+          Discover your benefits as a member
+        </p>
+      </div>
       <div className="membership-slider">
-        <MembershipCard imgUrl="https://cdn.govexec.com/media/img/cd/2023/11/17/11172023USPS/open-graph.jpg" title="Free Shipping"/>
-        <MembershipCard imgUrl="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/320x320/649840_v2.jpg" title="Early Access to shoes"/>
-        <MembershipCard imgUrl="https://www.xpresstags.com/img/lg/L/Circular-Sale-Price-Label-Sticker-LB-1795.gif" title="Membership Sales"/>
-        <MembershipCard imgUrl="https://www.hrosm.com/wp-content/uploads/2021/11/text-head.png" title="Membership Rewards"/>
-        <MembershipCard imgUrl="https://cdn.nextgov.com/media/img/cd/2020/12/09/NGshopping20201209/open-graph.jpg" title="Membership Exclusive Products"/>
+        <MembershipCard
+          imgUrl="https://cdn.govexec.com/media/img/cd/2023/11/17/11172023USPS/open-graph.jpg"
+          title="Free Shipping"
+        />
+        <MembershipCard
+          imgUrl="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/320x320/649840_v2.jpg"
+          title="Early Access to shoes"
+        />
+        <MembershipCard
+          imgUrl="https://www.xpresstags.com/img/lg/L/Circular-Sale-Price-Label-Sticker-LB-1795.gif"
+          title="Membership Sales"
+        />
+        <MembershipCard
+          imgUrl="https://www.hrosm.com/wp-content/uploads/2021/11/text-head.png"
+          title="Membership Rewards"
+        />
+        <MembershipCard
+          imgUrl="https://cdn.nextgov.com/media/img/cd/2020/12/09/NGshopping20201209/open-graph.jpg"
+          title="Membership Exclusive Products"
+        />
       </div>
       <div className="flex justify-center mt-10">
-        <ButtonRounded text="Become a member!"/>
+        <ButtonRounded text="Become a member!" />
       </div>
     </div>
   );
