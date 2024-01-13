4a80c0845e1830f81488b7a86419e1b7fc76fa3e
TonioCoding <ToneBone7960@gmail.com>
2024-01-13 15:08:52 -0500

    styled UI, homepage foundation is complete

--- client/src/components/homepage/RedirectSection.jsx
+++ client/src/components/homepage/RedirectSection.jsx
@@ -1,11 +1,12 @@
 const RedirectSection = () => {
   return (
     <div className="w-full">
-      <div className="flex">
-        <button className="redirect-button">BRANDS</button>
-        <button className="redirect-button">SALES</button>
-        <button className="redirect-button">MEMBERSHIP</button>
-        <button className="redirect-button">SPORTS</button>
+        <h1 className="text-center font-lt text-2xl mb-5 underline">GET STARTED</h1>
+      <div className="flex flex-col justify-center items-center sm:flex-row">
+        <button className="redirect-button text-3xl">BRANDS</button>
+        <button className="redirect-button text-3xl">SALES</button>
+        <button className="redirect-button text-3xl">MEMBERSHIP</button>
+        <button className="redirect-button text-3xl">SPORTS</button>
       </div>
     </div>
   );
