7da2317d20bba92639927641bbc40ca59e9e90a1
TonioCoding <ToneBone7960@gmail.com>
2024-01-12 22:05:29 -0500

    selected images for membership slider

--- client/src/components/homepage/MembershipSection.jsx
+++ client/src/components/homepage/MembershipSection.jsx
@@ -6,10 +6,10 @@ const MembershipSection = () => {
       <h1 className="text-center font-lt text-3xl mb-20">Membership</h1>
       <div className="membership-slider">
         <MembershipCard imgUrl="https://cdn.govexec.com/media/img/cd/2023/11/17/11172023USPS/open-graph.jpg" />
-        <MembershipCard imgUrl="https://animemangastore.com/wp-content/uploads/2022/12/men-shoes-sneakers-male-mens-casual-shoe_main-3-1-600x600.jpg" />
-        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
-        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
-        <MembershipCard imgUrl="https://static.nike.com/a/images/t_default/f8a45acc-7b34-49e0-9760-e1d070befcdb/pegasus-40-womens-road-running-shoes-L80k6C.png" />
+        <MembershipCard imgUrl="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/320x320/649840_v2.jpg" />
+        <MembershipCard imgUrl="https://www.xpresstags.com/img/lg/L/Circular-Sale-Price-Label-Sticker-LB-1795.gif" />
+        <MembershipCard imgUrl="https://www.hrosm.com/wp-content/uploads/2021/11/text-head.png" />
+        <MembershipCard imgUrl="https://cdn.nextgov.com/media/img/cd/2020/12/09/NGshopping20201209/open-graph.jpg" />
       </div>
       <div className="flex justify-center mt-10">
         <ButtonRounded text="Become a member!"/>
