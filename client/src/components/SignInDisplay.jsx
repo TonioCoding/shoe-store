fb0968cb8e9c126a7683f05057ef9c24c65de324
TonioCoding <ToneBone7960@gmail.com>
2024-01-24 17:09:50 -0500

    added state and submit function to signin display

--- client/src/components/SignInDisplay.jsx
+++ client/src/components/SignInDisplay.jsx
@@ -1,15 +1,24 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import {
+  Button,
   Dialog,
   DialogBody,
   DialogHeader,
   Input,
 } from "@material-tailwind/react";
+import { useState } from "react";
 const SignInDisplay = (props) => {
   const state = props.state;
   const buttonClose = props.buttonClose;
   const buttonSubmit = props.buttonSubmit;
   const buttonSignup = props.buttonSignup;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = () => {
+    
+  }
 
   return (
     <div id="sign-in" className="bg-black z-30 rounded-lg">
@@ -48,7 +57,7 @@ const SignInDisplay = (props) => {
           <div className="flex justify-center">
             {buttonSignup ? buttonSignup : null}
             {buttonClose ? buttonClose : null}
-            {buttonSubmit ? buttonSubmit : null}
+            <div onClick={handleSubmit}>{buttonSubmit ? buttonSubmit : null}</div>
           </div>
         </Dialog>
       ) : null}
