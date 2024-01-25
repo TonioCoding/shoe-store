687585ec71c8cdf3ef55f6060959fbd1671d2e52
TonioCoding <ToneBone7960@gmail.com>
2024-01-25 16:57:50 -0500

    created user api slice

---
+++ client/src/redux/user/userApiSlice.js
@@ -0,0 +1,37 @@
+import { apiSlice } from "../api/apiSlice";
+
+const USER_URL = "/api/v1/users";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USER_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    update: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useUpdateMutation} = userApiSlice
