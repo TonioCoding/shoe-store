5cd6b44fc24d6cf71f3b8c7400e1d05c23eacf52
TonioCoding <ToneBone7960@gmail.com>
2024-06-25 17:38:56 -0400

    created order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,12 +1,37 @@
-//Order model properties
+import mongoose from "mongoose";
 
+const orderSchema = mongoose.Schema(
+  {
+   status: {
+    type: String,
+    default: "pending"
+   },
+   totalPrice: {
+    type: Number,
+    required: true,
+   },
+   accountId : {
+    type: String,
+    required: true
+   },
+   orderItems: {
+    type: Array,
+    required: true,
+   },
+   shippingAddress: {
+    type: String,
+    required: true,
+   },
+   paymentMethod: {
+    type: String,
+    required: true,
+   }
+  },
+  {
+    timestamps: true,
+  }
+);
 
-/* 
--user
--date
--status
--total price
--items(array of objects)
--shipping address
--payment method
-*/
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
