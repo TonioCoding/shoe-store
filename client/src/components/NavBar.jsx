aa519dea1501a4d7760b75d42c31405310c1aa6b
TonioCoding <ToneBone7960@gmail.com>
2024-06-26 14:07:24 -0400

    configuring navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -155,7 +155,7 @@ const NavBar = () => {
   );
 
   return (
-    <header className="max-w-full sticky">
+    <header className="max-w-full">
       {showSignup === true ? (
         <SignUpDisplay
           state={showSignup}
@@ -212,21 +212,20 @@ const NavBar = () => {
         />
       ) : null}
       {/*BLACK STRIP*/}
-      <div className="bg-black w-full shadow-xl">
+      {/* <div className="bg-black w-full shadow-xl">
         <h1 className="text-center text-white pt-1 font-lt">Welcome!</h1>
         <p className="text-center text-white font-rt py-2">
           Winter sale, 45% off
         </p>
-      </div>
+      </div> */}
       {/*END OF STRIP*/}
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+      <Navbar className="fixed top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-blue-gray-900 basis-0">
           <Link to={"/"}>
             <Typography className="mr-4 cursor-pointer py-1.5 font-medium font-lt text-2xl text-black">
               Shoe Store
             </Typography>
           </Link>
-
           <div className="hidden sm:flex items-center">
             <NavSearchBar />
           </div>
@@ -348,29 +347,6 @@ const NavBar = () => {
           {wordList}
         </div>
       </Navbar>
-      
-      {/*CONTENT SECTION OF BOTTOM OF NAVBAR*/}
-      {/* {currentPage === "/" ? (
-        <div>
-          <div className="flex justify-center items-center mb-5 w-full overflow-x-clip shadow-2xl flex-col lg:flex-row bg-[#f5f5f5]">
-            <ImageWithShadow ImgUrl="https://stuffgy.com/cdn/shop/articles/kyrie7collage_1024x.jpg?v=1605625907" />
-            <ImageWithShadow ImgUrl="https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_xy_center%2Cq_auto:good%2Cw_1200%2Cx_3237%2Cy_3988/MjAyOTUxNTIzMzUxNjY3NzI0/edwards-adidas.jpg" />
-            <ImageWithShadow ImgUrl="https://static.ebayinc.com/static/assets/Uploads/Stories/Articles/_resampled/ScaleWidthWyIxMDI0Il0/melo2.jpg" />
-          </div>
-          <div className=" flex flex-col justify-center mb-30 text-">
-            <h1 className="text-center font-lt text-3xl m-3 mt-10">
-              Winter Basketball Shoe Sale
-            </h1>
-            <p className="text-center font-rt text-md">
-              45% markdown on Basketball sneakers
-            </p>
-            <div className="flex justify-center mt-10">
-              <LearnMoreButton text="View Shoes" />
-            </div>
-          </div>
-        </div>
-      ) : null} */}
-      {/*END OF CONTENT*/}
     </header>
   );
 };
