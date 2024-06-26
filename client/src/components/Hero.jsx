a0252ddc98f821da60dada00d1da3afa76671592
TonioCoding <ToneBone7960@gmail.com>
2024-06-26 15:38:51 -0400

    configured hero asset imgs

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
   return (
     <section className="w-full">
       <div className="flex justify-center items-center mb-5 max-w-full overflow-x-clip shadow-2xl flex-col lg:flex-row bg-[#f5f5f5] flex-shrink-1">
-        <ImageWithShadow ImgUrl="https://stuffgy.com/cdn/shop/articles/kyrie7collage_1024x.jpg?v=1605625907" />
-        <ImageWithShadow ImgUrl="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2023/09/28/16959382869511.jpg" />
-        <ImageWithShadow ImgUrl="https://static.ebayinc.com/static/assets/Uploads/Stories/Articles/_resampled/ScaleWidthWyIxMDI0Il0/melo2.jpg" />
+        <ImageWithShadow ImgUrl="../../src/assets/imgs/hero-imgs/kyrie7collage_1024x.webp" />
+        <ImageWithShadow ImgUrl="../../src/assets/imgs/hero-imgs/ae1photoshoot.jpg" />
+        <ImageWithShadow ImgUrl="../../src/assets/imgs/hero-imgs/melo2photoshoot.jpg" />
       </div>
       <div className=" flex flex-col justify-center mb-30 text-">
         <h1 className="text-center font-lt text-3xl m-3 mt-10">
