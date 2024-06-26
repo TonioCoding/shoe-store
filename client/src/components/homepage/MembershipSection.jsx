a608ae271704b3f0db5dd3805071f73898aded38
TonioCoding <ToneBone7960@gmail.com>
2024-06-26 17:08:02 -0400

    configured membership section assets

--- client/src/components/homepage/MembershipSection.jsx
+++ client/src/components/homepage/MembershipSection.jsx
@@ -11,27 +11,27 @@ const MembershipSection = () => {
       </div>
       <div className="membership-slider">
         <MembershipCard
-          imgUrl="https://cdn.govexec.com/media/img/cd/2023/11/17/11172023USPS/open-graph.jpg"
+          imgUrl="../src/assets/imgs/home-imgs/membership-img-1.jpg"
           title="Free Shipping"
           text="Zero shipping fees on any order"
         />
         <MembershipCard
-          imgUrl="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/320x320/649840_v2.jpg"
+          imgUrl="../src/assets/imgs/home-imgs/membership-img-2.jpg"
           title="Early Access to shoes"
           text="Gain access eariler to the latest shoes"
         />
         <MembershipCard
-          imgUrl="https://www.xpresstags.com/img/lg/L/Circular-Sale-Price-Label-Sticker-LB-1795.gif"
+          imgUrl="../src/assets/imgs/home-imgs/membership-img-3.gif"
           title="Membership Sales"
           text="Exclusive sales to members only"
         />
         <MembershipCard
-          imgUrl="https://www.hrosm.com/wp-content/uploads/2021/11/text-head.png"
+          imgUrl="../src/assets/imgs/home-imgs/membership-img-4.png"
           title="Membership Rewards"
           text="Experience rewards as a loyal member"
         />
         <MembershipCard
-          imgUrl="https://cdn.nextgov.com/media/img/cd/2020/12/09/NGshopping20201209/open-graph.jpg"
+          imgUrl="../src/assets/imgs/home-imgs/membership-img-5.jpg"
           title="Membership Exclusive Products"
           text="Exclusive products to members only"
         />
