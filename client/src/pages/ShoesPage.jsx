fc3bda683b87531a53bf16b02a0e032938a1acb1
TonioCoding <ToneBone7960@gmail.com>
2024-06-30 01:29:48 -0400

    configuring shoe page functionality

--- client/src/pages/ShoesPage.jsx
+++ client/src/pages/ShoesPage.jsx
@@ -1,29 +1,53 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 import { Typography } from "@material-tailwind/react";
 import ShoeCard from "../components/ShoeCard";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { PropTypes } from "prop-types";
 
 const ShoePage = ({ brand }) => {
   const [shoeData, setShoeData] = useState(null);
+  const [currentBrand, setCurrentBrand] = useState(brand);
   console.log(shoeData);
   console.log(brand);
+  console.log(currentBrand);
 
   useEffect(() => {
     async function retrieveShoes() {
-      if (shoeData === null) {
+      let fetchUrl = "";
+      switch (currentBrand) {
+        case "Nike":
+          fetchUrl = "http://localhost:9000/api/v1/shoe/get-shoes/Nike";
+          break;
+        case "Jordan":
+          fetchUrl = "http://localhost:9000/api/v1/shoe/get-shoes/Jordan";
+          break;
+        case "New Balance":
+          fetchUrl = "http://localhost:9000/api/v1/shoe/get-shoes/NewBalance";
+          break;
+        case "Adidas":
+          fetchUrl = "http://localhost:9000/api/v1/shoe/get-shoes/Adidas";
+          break;
+        case "Puma":
+          fetchUrl = "http://localhost:9000/api/v1/shoe/get-shoes/Puma";
+          break;
+        case "Reebok":
+          fetchUrl = "http://localhost:9000/api/v1/shoe/get-shoes/Reebok";
+          break;
+      }
+
+      if (currentBrand) {
         try {
-          const req = fetch(
-            `http://localhost:9000/api/v1/shoe/get-shoes/${brand}`,
-            {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
+          const req = fetch(fetchUrl, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
 
           const res = await req.json();
+          console.log(res);
           setShoeData(res);
         } catch (error) {
           toast.error(error);
@@ -32,7 +56,7 @@ const ShoePage = ({ brand }) => {
     }
 
     retrieveShoes();
-  }, [shoeData]);
+  }, [shoeData, currentBrand]);
   return (
     <main className="bg-white h-[100vh]">
       <section className="pt-4 px-5">
@@ -47,6 +71,10 @@ const ShoePage = ({ brand }) => {
       </section>
     </main>
   );
+};
+
+ShoePage.propTypes = {
+  brand: PropTypes.string,
 };
 
 export default ShoePage;
