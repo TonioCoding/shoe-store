f84d9806e6ddc9c46a39fed3d97d4ea5ae3fc984
TonioCoding <ToneBone7960@gmail.com>
2024-09-02 18:17:34 -0400

    configuring redux state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import authReducer from "../redux/auth/authSlice.js";
+import cartReducer from "../redux/cart/cartSlice.js";
 import { thunk } from "redux-thunk";
 import { apiSlice } from "./api/apiSlice.js";
 
@@ -11,7 +12,8 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
-    auth: authReducer,
+  auth: authReducer,
+  cart: cartReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -19,12 +21,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
   reducer: {
     persistedReducer,
-    [apiSlice.reducerPath] : apiSlice.reducer
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
   devTools: true,
   // eslint-disable-next-line no-unused-vars
   middleware: (getDefaultMiddleware) => {
-    return [thunk]
+    return [thunk];
   },
 });
 
