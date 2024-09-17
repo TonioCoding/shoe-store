9762ffece50764828b5fc479e9fbe222aaee8097
TonioCoding <ToneBone7960@gmail.com>
2024-09-17 16:06:59 -0400

    configuring interest card component

--- client/src/components/accountpage/InterestCard.jsx
+++ client/src/components/accountpage/InterestCard.jsx
@@ -6,12 +6,15 @@ function InterestCard(props) {
   const text = props.text;
 
   return (
-    <div className="w-[15%] h-[35%] shadow-2xl flex-col text-center inline-flex relative cursor-pointer">
+    <div className="interest-card w-[15%] h-[35%] shadow-2xl flex-col text-center inline-flex relative cursor-pointer bg-black">
       <img
         src={imgSrc}
         className="max-w-full min-h-full object-cover object-center"
       />
-      <Typography className="absolute bottom-0 ml-2 mb-2 text-white font-lt bg-gray-900 border-[1.5px] border-white p-2 px-4 rounded-3xl text-sm hover:bg-white hover:text-black transition-all ease-in-out duration-500 shadow-inner">
+      <Typography
+        variant="h6"
+        className="absolute bottom-0 ml-2 mb-2 text-white font-lt border-[1.5px] border-white p-1 px-3 rounded-3xl hover:bg-white hover:text-black transition-all ease-in-out duration-500 shadow-inner"
+      >
         {text}
       </Typography>
     </div>
