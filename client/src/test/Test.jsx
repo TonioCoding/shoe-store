ae5339c9f221967cc5d1f3ec58767364bedad1c2
TonioCoding <ToneBone7960@gmail.com>
2024-09-19 14:22:03 -0400

    removed margin from test page

--- client/src/test/Test.jsx
+++ client/src/test/Test.jsx
@@ -7,7 +7,7 @@ import InterestCard from "../components/accountpage/InterestCard";
 
 const Test = () => {
   return (
-    <div className="items-center flex h-[100vh] gap-x-7 ml-2">
+    <div className="items-center flex h-[100vh] gap-x-7">
       <InterestCard img={basketballInterestImg} text="Basketball" />
       <InterestCard img={baseballInterestImg} text="Baseball" />
       <InterestCard img={soccerInterestImg} text="Soccer" />
