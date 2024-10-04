851362d23dcc4aee937cbee930880b0f3b3a154d
TonioCoding <ToneBone7960@gmail.com>
2024-10-04 13:06:31 -0400

    creating delivery addresses dialog

--- client/src/components/accountpage/DeliveryAddressesDialog.jsx
+++ client/src/components/accountpage/DeliveryAddressesDialog.jsx
@@ -1,8 +1,10 @@
 import {
+  Button,
   Dialog,
   DialogBody,
   DialogFooter,
   DialogHeader,
+  Input,
   Typography,
 } from "@material-tailwind/react";
 import { VscChromeClose } from "react-icons/vsc";
@@ -14,7 +16,11 @@ const DeliveryAddressesDialog = (props) => {
   const handleDialog = props.handleDialog;
 
   return (
-    <Dialog open={open}>
+    <Dialog
+      open={open}
+      size="xs"
+      className="h-[65vh] overscroll-contain overflow-y-scroll"
+    >
       <DialogHeader>
         <Typography className="font-lt" variant="h5" color="black">
           Add Address
@@ -26,8 +32,115 @@ const DeliveryAddressesDialog = (props) => {
           />
         </IconContext.Provider>
       </DialogHeader>
-      <DialogBody></DialogBody>
-      <DialogFooter></DialogFooter>
+      <DialogBody>
+        <div className="w-full flex">
+          <Input
+            label="Current Password*"
+            type="number"
+            placeholder="CVV"
+            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "max-w-[20%]" }}
+          />
+          <Input
+            label="Current Password*"
+            type="number"
+            placeholder="CVV"
+            className="w-[50%] !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+          />
+        </div>
+        <Input
+          label="Current Password*"
+          type="number"
+          placeholder="CVV"
+          className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+          labelProps={{
+            className: "hidden",
+          }}
+          containerProps={{ className: "min-w-[100px]" }}
+        />
+        <Input
+          label="Current Password*"
+          type="number"
+          placeholder="CVV"
+          className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+          labelProps={{
+            className: "hidden",
+          }}
+          containerProps={{ className: "min-w-[100px]" }}
+        />
+        <div className="">
+          <Input
+            label="Current Password*"
+            type="number"
+            placeholder="CVV"
+            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+          />
+          <Input
+            label="Current Password*"
+            type="number"
+            placeholder="CVV"
+            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+          />
+        </div>
+        <div className="">
+          <Input
+            label="Current Password*"
+            type="number"
+            placeholder="CVV"
+            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+          />
+          <Input
+            label="Current Password*"
+            type="number"
+            placeholder="CVV"
+            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+          />
+        </div>
+        <Input
+          label="Current Password*"
+          type="number"
+          placeholder="CVV"
+          className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+          labelProps={{
+            className: "hidden",
+          }}
+          containerProps={{ className: "min-w-[100px]" }}
+        />
+        <div className="flex gap-x-3">
+          <input type="checkbox" />
+          <Typography className="font-rt text-black text-base">
+            Set as default payment method
+          </Typography>
+        </div>
+      </DialogBody>
+      <DialogFooter>
+        <Button className="rounded-full self-end my-4 bg-gray-400 text-gray-700 mr-5">
+          Save
+        </Button>
+      </DialogFooter>
     </Dialog>
   );
 };
