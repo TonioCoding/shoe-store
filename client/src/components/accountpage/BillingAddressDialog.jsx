742e22aa1d1baefbf08235327b028248534d73fe
TonioCoding <ToneBone7960@gmail.com>
2024-10-11 12:08:57 -0400

    creating add payment method functionality

---
+++ client/src/components/accountpage/BillingAddressDialog.jsx
@@ -0,0 +1,128 @@
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  Input,
+  Typography,
+} from "@material-tailwind/react";
+import { IconContext } from "react-icons";
+import { VscChromeClose } from "react-icons/vsc";
+import PropTypes from "prop-types";
+
+const BillingAddressDialog = (props) => {
+  const open = props.open;
+  const handleDialog = props.handleDialog;
+  return (
+    <Dialog
+      open={open}
+      size="xs"
+      className="h-[65vh] overscroll-contain overflow-y-scroll px-5"
+    >
+      <DialogHeader className="flex justify-between">
+        <Typography className="font-lt" variant="h5" color="black">
+          Billing Address
+        </Typography>
+        <IconContext.Provider value={{ size: "2rem" }}>
+          <VscChromeClose
+            className="cursor-pointer text-gray-800 bg-gray-200 rounded-full p-1"
+            onClick={handleDialog}
+          />
+        </IconContext.Provider>
+      </DialogHeader>
+      <DialogBody className="flex flex-col gap-y-5 items-center">
+        <div className="flex flex-col gap-y-4 items-center sm:flex-row gap-x-5 justify-center">
+          <input
+            label="Current Password*"
+            type="text"
+            placeholder="First Name*"
+            className="min-w-[150px] w-[35%] !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 placeholder:text-sm focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-md pl-3 p-1"
+          />
+          <input
+            label="Current Password*"
+            type="text"
+            placeholder="Last Name*"
+            className="min-w-[150px] w-[35%] !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 placeholder:text-sm focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-md pl-3 p-1"
+          />
+        </div>
+        <Input
+          label="Current Password*"
+          type="text"
+          placeholder="Street Address*"
+          className="self-center !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+          labelProps={{
+            className: "hidden",
+          }}
+          containerProps={{ className: "min-w-[100px]" }}
+        />
+        <Input
+          label="Current Password*"
+          type="number"
+          placeholder="Apt, Suite, Building"
+          className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+          labelProps={{
+            className: "hidden",
+          }}
+          containerProps={{ className: "min-w-[100px]" }}
+        />
+        <div className="flex flex-col gap-y-4 items-center sm:flex-row gap-x-5 justify-center">
+          <input
+            label="Current Password*"
+            type="number"
+            placeholder="City*"
+            className="min-w-[150px] placeholder:text-sm w-[35%] !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-md pl-3 p-1"
+          />
+          <input
+            label="Current Password*"
+            type="number"
+            placeholder="Zip*"
+            className="min-w-[150px] placeholder:text-sm w-[35%] !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-md pl-3 p-1"
+          />
+        </div>
+        <div className="flex flex-col gap-y-4 items-center sm:flex-row gap-x-5 justify-center">
+          <input
+            label="Current Password*"
+            type="number"
+            placeholder="State*"
+            className="min-w-[150px] placeholder:text-sm w-[35%] !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-md pl-3 p-1"
+          />
+          <input
+            label="Current Password*"
+            type="number"
+            placeholder="Country/Region*"
+            className="min-w-[150px] placeholder:text-sm w-[35%] !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-md pl-3 p-1"
+          />
+        </div>
+        {/* <Input
+          label="Current Password*"
+          type="number"
+          placeholder="Phone Number*"
+          className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+          labelProps={{
+            className: "hidden",
+          }}
+          containerProps={{ className: "min-w-[100px]" }}
+        /> */}
+        <div className="flex gap-x-3 self-start">
+          <input type="checkbox" />
+          <Typography className="font-rt text-black text-base">
+            Set as default shipping address
+          </Typography>
+        </div>
+      </DialogBody>
+      <DialogFooter className="p-0">
+        <Button className="rounded-full self-end my-4 bg-gray-400 text-gray-700 mr-5">
+          Save
+        </Button>
+      </DialogFooter>
+    </Dialog>
+  );
+};
+
+BillingAddressDialog.propTypes = {
+  open: PropTypes.bool,
+  handleDialog: PropTypes.func,
+};
+
+export default BillingAddressDialog;
--- client/src/components/accountpage/PaymentMethodsDialog.jsx
+++ client/src/components/accountpage/PaymentMethodsDialog.jsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { IconContext } from "react-icons";
 import { VscChromeClose } from "react-icons/vsc";
 import { toast } from "react-toastify";
+import BillingAddressDialog from "./BillingAddressDialog";
 
 const PaymentMethodsDialog = (props) => {
   const handleDialog = props.handleDialog;
@@ -22,6 +23,8 @@ const PaymentMethodsDialog = (props) => {
     expirationDate: null,
     cvv: null,
   });
+  const [billingAddress, setBillingAddress] = useState(null);
+  const [openBillingAddress, setOpenBillingAddress] = useState(false);
 
   async function addPaymentMethod() {
     if (
@@ -50,99 +53,120 @@ const PaymentMethodsDialog = (props) => {
     }
   }
 
+  function handleBillingAddressDialog() {
+    openBillingAddress === false;
+
+    if (openBillingAddress === false) {
+      setOpenBillingAddress(true);
+      handleDialog();
+    } else {
+      setOpenBillingAddress(false);
+      handleDialog();
+    }
+  }
+
   return (
-    <Dialog
-      open={open}
-      size="xs"
-      className="h-fit overscroll-contain overflow-y-auto"
-    >
-      <DialogHeader className="flex justify-between relative">
-        <Typography className="font-lt" variant="h5" color="black">
-          Add Payment Method
-        </Typography>
-        <IconContext.Provider value={{ size: "2rem" }}>
-          <VscChromeClose
-            onClick={handleDialog}
-            className="cursor-pointer text-gray-800 bg-gray-200 rounded-full p-1"
-          />
-        </IconContext.Provider>
-      </DialogHeader>
-      <DialogBody>
-        <div className="flex flex-col gap-y-4 border p-4 border-gray-300">
-          <Input
-            onChange={(e) => {
-              setPaymentMethod((prev) => ({
-                ...prev,
-                cardNumber: e.target.value,
-              }));
-            }}
-            type="number"
-            placeholder="Card Number"
-            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
-            labelProps={{
-              className: "hidden",
-            }}
-            containerProps={{ className: "min-w-[100px]" }}
-          />
-          <Input
-            onChange={(e) => {
-              setPaymentMethod((prev) => ({
-                ...prev,
-                expirationDate: e.target.value,
-              }));
-            }}
-            type="month"
-            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
-            labelProps={{
-              className: "hidden",
-            }}
-            containerProps={{ className: "min-w-[100px]" }}
-          />
-          <Input
-            onChange={(e) => {
-              setPaymentMethod((prev) => ({
-                ...prev,
-                cvv: e.target.value,
-              }));
-            }}
-            type="number"
-            placeholder="CVV"
-            className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
-            labelProps={{
-              className: "hidden",
-            }}
-            containerProps={{ className: "min-w-[100px]" }}
-          />
-        </div>
-        <Button className="mt-5 rounded-full bg-white text-black border border-gray-300 hover:border-black">
-          <Typography className="text-xs font-rt">
-            Add Billing Address
+    <>
+      <BillingAddressDialog
+        open={openBillingAddress}
+        handleDialog={handleBillingAddressDialog}
+      />
+      <Dialog
+        open={open}
+        size="xs"
+        className="h-fit overscroll-contain overflow-y-auto"
+      >
+        <DialogHeader className="flex justify-between relative">
+          <Typography className="font-lt" variant="h5" color="black">
+            Add Payment Method
           </Typography>
-        </Button>
-        <div className="flex flex-col my-8 ml-10">
-          <div className="flex gap-x-3">
-            <input type="checkbox" />
-            <Typography className="font-rt text-black text-base">
-              Billing address same as default shipping
-            </Typography>
+          <IconContext.Provider value={{ size: "2rem" }}>
+            <VscChromeClose
+              onClick={handleDialog}
+              className="cursor-pointer text-gray-800 bg-gray-200 rounded-full p-1"
+            />
+          </IconContext.Provider>
+        </DialogHeader>
+        <DialogBody>
+          <div className="flex flex-col gap-y-4 border p-4 border-gray-300">
+            <Input
+              onChange={(e) => {
+                setPaymentMethod((prev) => ({
+                  ...prev,
+                  cardNumber: e.target.value,
+                }));
+              }}
+              type="number"
+              placeholder="Card Number"
+              className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+              labelProps={{
+                className: "hidden",
+              }}
+              containerProps={{ className: "min-w-[100px]" }}
+            />
+            <Input
+              onChange={(e) => {
+                setPaymentMethod((prev) => ({
+                  ...prev,
+                  expirationDate: e.target.value,
+                }));
+              }}
+              type="month"
+              className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+              labelProps={{
+                className: "hidden",
+              }}
+              containerProps={{ className: "min-w-[100px]" }}
+            />
+            <Input
+              onChange={(e) => {
+                setPaymentMethod((prev) => ({
+                  ...prev,
+                  cvv: e.target.value,
+                }));
+              }}
+              type="number"
+              placeholder="CVV"
+              className="!border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+              labelProps={{
+                className: "hidden",
+              }}
+              containerProps={{ className: "min-w-[100px]" }}
+            />
           </div>
-          <div className="flex gap-x-3">
-            <input type="checkbox" />
-            <Typography className="font-rt text-black text-base">
-              Set as default payment method
+          <Button
+            className="mt-5 rounded-full bg-white text-black border border-gray-300 hover:border-black"
+            onClick={handleBillingAddressDialog}
+          >
+            <Typography className="text-xs font-rt">
+              Add Billing Address
             </Typography>
+          </Button>
+          <div className="flex flex-col my-8 ml-10">
+            <div className="flex gap-x-3">
+              <input type="checkbox" />
+              <Typography className="font-rt text-black text-base">
+                Billing address same as default shipping
+              </Typography>
+            </div>
+            <div className="flex gap-x-3">
+              <input type="checkbox" />
+              <Typography className="font-rt text-black text-base">
+                Set as default payment method
+              </Typography>
+            </div>
           </div>
-        </div>
-      </DialogBody>
-      <DialogFooter className="p-0">
-        <Button
-          className="rounded-full self-end my-4 bg-gray-400 text-gray-700 mr-5"
-          onClick={addPaymentMethod}
-        >
-          Save
-        </Button>
-      </DialogFooter>
-    </Dialog>
+        </DialogBody>
+        <DialogFooter className="p-0">
+          <Button
+            className="rounded-full self-end my-4 bg-gray-400 text-gray-700 mr-5"
+            onClick={addPaymentMethod}
+          >
+            Save
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
   );
 };
 
