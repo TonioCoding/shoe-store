866fba80bf7c18770704bb9ad38b0aebcb4aaf93
TonioCoding <ToneBone7960@gmail.com>
2024-10-13 23:01:18 -0400

    removed shadows from brand cards

--- client/src/components/HomePageCard.jsx
+++ client/src/components/HomePageCard.jsx
@@ -11,14 +11,21 @@ import ButtonRounded from "./RoundedButton";
 const HomeCard = (props) => {
   return (
     <Card className="w-[55vw] m-5 lg:w-[25vw] rounded-none" id="card">
-      <CardHeader floated={false} className="lg:h-[35vh] flex items-center justify-center rounded-none">
-        <img src={props.src} alt="picture" className="object-cover object-center"/>
+      <CardHeader
+        floated={false}
+        className="lg:h-[35vh] flex items-center justify-center rounded-none shadow-none"
+      >
+        <img
+          src={props.src}
+          alt="picture"
+          className="object-cover object-center"
+        />
       </CardHeader>
       <CardBody className="text-center">
         <Typography variant="h4" color="black" className="mb-2 font-lt">
           {props.brandName}
         </Typography>
-        <ButtonRounded text="Shop now!" className="text-center"/>
+        <ButtonRounded text="Shop now!" className="text-center" />
       </CardBody>
     </Card>
   );
