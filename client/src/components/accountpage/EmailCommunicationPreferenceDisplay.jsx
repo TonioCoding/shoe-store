874035640d7e52332efc4c9a518923e97ccdd46f
TonioCoding <ToneBone7960@gmail.com>
2024-10-13 20:57:42 -0400

    created communication preference functionality

--- client/src/components/accountpage/EmailCommunicationPreferenceDisplay.jsx
+++ client/src/components/accountpage/EmailCommunicationPreferenceDisplay.jsx
@@ -6,7 +6,36 @@ import { toast } from "react-toastify";
 const EmailCommunicationPreferenceDisplay = () => {
   const { userInfo } = useSelector((state) => state.persistedReducer.auth);
   const [sendEmails, setSendEmails] = useState(false);
-  console.log(sendEmails);
+
+  async function confirmSendEmails() {
+    if (sendEmails === false) {
+      try {
+        const req = fetch("/api/v1/users/confirmCommunicationPreferences", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userId: userInfo._id,
+          }),
+        });
+
+        const res = (await req)
+          .json()
+          .then((data) => {
+            setSendEmails(data);
+          })
+          .then(toast.success("Communication preferences have been updated"))
+          .catch((err) => {
+            throw new Error(err);
+          });
+      } catch (error) {
+        toast.error(error);
+      }
+    } else {
+      toast.error("Sending emails to this account is already confirmed");
+    }
+  }
 
   useEffect(() => {
     async function communicationPreferences() {
@@ -28,7 +57,6 @@ const EmailCommunicationPreferenceDisplay = () => {
         const res = (await checkCommunicationPreferences)
           .json()
           .then((data) => {
-            console.log(data);
             setSendEmails(data);
           })
           .catch((err) => {
@@ -40,6 +68,7 @@ const EmailCommunicationPreferenceDisplay = () => {
     }
     communicationPreferences();
   }, [sendEmails, userInfo]);
+
   return (
     <div className="flex flex-col w-fit gap-y-5">
       <Typography className="font-lt" variant="h5">
@@ -50,7 +79,12 @@ const EmailCommunicationPreferenceDisplay = () => {
         Get updates on products&#44; offers&#44; and your member benefits&#46;
       </Typography>
       <div className="flex items-center gap-x-3">
-        <input type="checkbox" checked={sendEmails} disabled={sendEmails} />
+        <input
+          type="checkbox"
+          checked={sendEmails}
+          disabled={sendEmails}
+          onClick={confirmSendEmails}
+        />
         <Typography className="font-rt text-sm">
           Yes&#44; send me emails&#46;
         </Typography>
