c94f8eb2ae93c60912fc7634a355ce93882cbcee
TonioCoding <ToneBone7960@gmail.com>
2024-10-16 18:45:28 -0400

    styling size badge

--- client/src/components/SizeBadge.jsx
+++ client/src/components/SizeBadge.jsx
@@ -9,10 +9,10 @@ const SizeBadge = (props) => {
   const sizesNotInStock = props.sizesNotInStock;
   const [sizeInStock, setSizeInStock] = useState(true);
   let className =
-    "size-badge py-2 px-5 my-1 min-w-[25vw] lg:min-w-[10vw] max-w-[10vw] border-2 border-gray-400 inline-flex items-center justify-center rounded-md hover:border-black transition-all duration-200 ease-in-out hover:cursor-pointer";
+    "size-badge py-2 px-5 my-1 min-w-[25vw] lg:min-w-[10vw] max-w-[10vw] border border-gray-400 inline-flex items-center justify-center rounded-md hover:border-black transition-all duration-200 ease-in-out hover:cursor-pointer";
   if (currentSize === size) {
     className =
-      "size-badge py-2 px-5 my-1 min-w-[25vw] lg:min-w-[10vw] max-w-[10vw] border-2 border-black inline-flex items-center justify-center rounded-md hover:border-black transition-all duration-200 ease-in-out hover:cursor-pointer";
+      "size-badge py-2 px-5 my-1 min-w-[25vw] lg:min-w-[10vw] max-w-[10vw] border border-black inline-flex items-center justify-center rounded-md hover:border-black transition-all duration-200 ease-in-out hover:cursor-pointer";
   }
 
   useEffect(() => {
@@ -29,7 +29,7 @@ const SizeBadge = (props) => {
   return (
     <>
       {sizeInStock === false ? (
-        <div className="bg-gray-200 size-badge py-2 px-5 my-1 min-w-[25vw] lg:min-w-[10vw] max-w-[10vw] border-2 border-gray-400 inline-flex items-center justify-center rounded-md transition-all duration-200 ease-in-out hover:cursor-not-allowed">
+        <div className="bg-gray-200 size-badge py-2 px-5 my-1 min-w-[25vw] lg:min-w-[10vw] max-w-[10vw] border border-gray-400 inline-flex items-center justify-center rounded-md transition-all duration-200 ease-in-out hover:cursor-not-allowed">
           <Typography variant="h6" className="font-normal text-gray-500">
             M {size} &#47; W {size + 1.5}
           </Typography>
