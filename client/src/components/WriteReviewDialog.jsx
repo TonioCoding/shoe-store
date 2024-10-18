9c6f9886656d95f1835d1bc4e83eac330faa5bf1
TonioCoding <ToneBone7960@gmail.com>
2024-10-18 17:28:54 -0400

    creating write review dialog

--- client/src/components/WriteReviewDialog.jsx
+++ client/src/components/WriteReviewDialog.jsx
@@ -3,7 +3,6 @@ import {
   DialogBody,
   DialogFooter,
   DialogHeader,
-  Input,
   Typography,
 } from "@material-tailwind/react";
 import PropTypes from "prop-types";
@@ -37,7 +36,7 @@ const WriteReviewDialog = (props) => {
     <Dialog
       size="md"
       open={open}
-      className="p-7 overscroll-contain overflow-y-scroll"
+      className="p-7 h-[75vh] !overscroll-contain !overflow-y-scroll"
     >
       <DialogHeader className="flex flex-col w-full">
         <IconContext.Provider value={{ size: "2rem" }}>
@@ -86,23 +85,50 @@ const WriteReviewDialog = (props) => {
             </IconContext.Provider>
           </div>
           <hr className="my-10 border-gray-500" />
-          <div className="flex flex-col gap-y-2">
+          <div className="flex flex-col gap-y-2 my-5">
             <Typography className="text-gray-800 items-center">
               Your Review <span className="text-red-600">&#42;</span>
             </Typography>
-            <Input
-              type="text"
-              className="!border  !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
-              labelProps={{
-                className: "hidden",
-              }}
-              containerProps={{ className: "min-w-[100px]" }}
-            />
-            <Typography>
+            <textarea className="rounded-lg px-2 focus:border-0 focus:outline-none !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"></textarea>
+            <Typography className="text-xs font-rt text-gray-500 flex items-center gap-x-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="-mt-px h-4 w-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
               Describe what you liked, what you didn&#39;t like, and other key
               things shoppers should know. Minimum 30 characters&#46;
             </Typography>
           </div>
+          <div className="my-5 flex flex-col gap-y-2">
+            <Typography className="text-gray-800 items-center">
+              Review Subject&#47;Title
+            </Typography>
+            <input className="px-2 w-full h-[7vh] rounded-lg focus:border-0 focus:outline-none !border !border-gray-500 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10" />
+            <Typography className="text-xs font-rt text-gray-500 flex items-center gap-x-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="-mt-px h-4 w-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Summarize your review in 150 characters or less&#46;
+            </Typography>
+          </div>
+          <hr className="my-10 border-gray-500" />
         </div>
       </DialogBody>
       <DialogFooter></DialogFooter>
